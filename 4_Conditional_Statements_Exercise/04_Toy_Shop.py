1ea65614743a5fce2653a8158b8fd47fd65ae58c
Georgi <gegata8797@gmail.com>
2025-09-23 18:01:43 +0300

    1

--- 4_Conditional_Statements_Exercise/04_Toy_Shop.py
+++ 4_Conditional_Statements_Exercise/04_Toy_Shop.py
@@ -1,19 +1,42 @@
-excursion = int(input())
-number_puzzle = int(input())
-number_dolls = int(input())
-number_bears = int(input())
-number_minions = int(input())
-number_trucks = int(input())
-# цени:
-price_puzzle = 2.60
-price_doll = 3
-price_bear = 4.10
-price_minion = 8.20
-price_truck = 2
-# сметки:
-order_price = ((number_puzzle * number_puzzle) + (number_dolls * number_dolls)
-               + (number_bears * number_bears) + (number_minions * number_minions) + (number_trucks * number_trucks))
-order_toys = number_puzzle + number_dolls + number_bears + number_minions + number_trucks
-if order_toys >= 50:
-    discount = order_price * 0.25
-rent =
+# Вход
+excursion_price = float(input())
+puzzles = int(input())
+talking_dolls = int(input())
+teddy_bears = int(input())
+minions = int(input())
+trucks = int(input())
+
+# Цени на играчките
+puzzle_price = 2.60
+talking_doll_price = 3.00
+teddy_bear_price = 4.10
+minion_price = 8.20
+truck_price = 2.00
+
+# Изчисляване на общата сума
+total_price = (
+    puzzles * puzzle_price +
+    talking_dolls * talking_doll_price +
+    teddy_bears * teddy_bear_price +
+    minions * minion_price +
+    trucks * truck_price
+)
+
+# Общо брой на играчките
+total_toys = puzzles + talking_dolls + teddy_bears + minions + trucks
+
+# Проверка за отстъпка
+if total_toys >= 50:
+    total_price *= 0.75  # 25% отстъпка
+
+# Наем 10%
+rent = total_price * 0.10
+profit = total_price - rent
+
+# Проверка дали парите стигат
+if profit >= excursion_price:
+    money_left = profit - excursion_price
+    print(f"Yes! {money_left:.2f} lv left.")
+else:
+    money_needed = excursion_price - profit
+    print(f"Not enough money! {money_needed:.2f} lv needed.")
